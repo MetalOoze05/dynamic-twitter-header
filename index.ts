61a3e2150bb196daa3d8237bc2e2750a40cec3b9
MetalOoze <38457291+MetalOoze05@users.noreply.github.com>
2022-07-21 22:32:27 +0530

    updates twitter banner dynamically

--- index.ts
+++ index.ts
@@ -1,10 +1,39 @@
-import { Client } from "twitter-api-sdk"
+import * as dotenv from "dotenv"
+import Twit from "twit"
 import chalk from "chalk"
 import fs from "node:fs"
 import { renderHeader } from "./src/renderHeader"
 
-const out = fs.createWriteStream(__dirname + "/out.png")
+dotenv.config()
 
-renderHeader(out)
+const twitter = new Twit({
+    consumer_key: `${process.env.CONSUMER_KEY}`,
+    consumer_secret: `${process.env.CONSUMER_SECRET}`,
+    access_token: `${process.env.ACCESS_TOKEN}`,
+    access_token_secret: `${process.env.ACCESS_TOKEN_SECRET}`,
+    timeout_ms: 60*1000,
+    strictSSL: true
+})
+
+const out = fs.createWriteStream(__dirname + "/out.png");
+
+(async () => {
+    renderHeader(out)
+
+    await new Promise((resolve, reject) => setTimeout(resolve, 3000))
+
+    const b64content = fs.readFileSync(__dirname + "/out.png", { encoding: "base64" })
+
+    twitter.post("media/upload", { media_data: b64content }, function (err, data: any, response) {
+        let media_id = data.media_id_string
+        twitter.post("account/update_profile_banner", { media_id: media_id }, function (err, data, response) {
+            if (err) {
+                console.error(err)
+            } else {
+                console.log(chalk.green("Updated twitter header!"))
+            }
+        })
+    })
+})()
 
 out.on("finish", () => console.log(chalk.green("Finished saving the png!")))
