762cafbc23094962a18cc120a87a6677056838a8
MetalOoze <38457291+MetalOoze05@users.noreply.github.com>
2022-07-21 19:12:42 +0530

    better code

--- index.ts
+++ index.ts
@@ -1,48 +1,10 @@
 import { Client } from "twitter-api-sdk"
 import chalk from "chalk"
-import { getQuote } from "./src/getQuote"
-import { createCanvas, registerFont } from "canvas"
 import fs from "node:fs"
-import drawMultilineText from "canvas-multiline-text"
-
-registerFont('./AurulentSansMono.otf', { family: 'myFont' })
+import { renderHeader } from "./src/renderHeader"
 
 const out = fs.createWriteStream(__dirname + "/out.png")
 
-const main = async () => {
-    const data = await getQuote()
-    console.log(chalk.gray(`Quote: ${data.text}\nAuthor: ${data.author}`))
-
-    const canvas = createCanvas(3000, 1000)
-    const ctx = canvas.getContext("2d")
-    
-    ctx.font = "myFont"
-    /*
-    ctx.fillText(data.text, 1500, 500, 2000)
-    ctx.fillText(`~ ${data.author}`, 1500, 600)
-    */
-   
-    ctx.textAlign = "center"
-    ctx.fillStyle = "#4A5568"
-
-    drawMultilineText(ctx, `"${data.text}" ~ ${data.author}`, {
-        rect: {
-            x: 1500, y: 400,
-            width: canvas.width - 300,
-            height: canvas.height - 100
-        },
-        font: 'myFont',
-        verbose: false,
-        lineHeight: 1.4,
-        minFontSize: 50,
-        maxFontSize: 70
-    })
-    
-    const stream = canvas.createJPEGStream()
-    
-    stream.pipe(out)    
-}
-
-main()
+renderHeader(out)
 
 out.on("finish", () => console.log(chalk.green("Finished saving the png!")))
---
+++ src/renderHeader.ts
@@ -0,0 +1,35 @@
+import chalk from "chalk"
+import { getQuote } from "./getQuote"
+import { createCanvas, registerFont } from "canvas"
+import drawMultilineText from "canvas-multiline-text"
+import type { WriteStream } from "node:fs"
+
+registerFont('./AurulentSansMono.otf', { family: "myFont" })
+
+export async function renderHeader(out: WriteStream) {
+    const data = await getQuote()
+    console.log(chalk.gray(`Quote: ${data.text}\nAuthor: ${data.author}`))
+
+    const canvas = createCanvas(3000, 1000)
+    const ctx = canvas.getContext("2d")
+
+    ctx.font = "myFont"
+    ctx.textAlign = "center"
+    ctx.fillStyle = "#4A5568"
+
+    drawMultilineText(ctx, `"${data.text}" ~ ${data.author}`, {
+        rect: {
+            x: 1500, y: 400,
+            width: canvas.width - 300,
+            height: canvas.height - 100
+        },
+        font: 'myFont',
+        verbose: false,
+        lineHeight: 1.4,
+        minFontSize: 50,
+        maxFontSize: 70
+    })
+
+    const stream = canvas.createJPEGStream()
+    stream.pipe(out)
+}
