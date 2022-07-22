42896de4b9f34dba2637fd410b732e0e2afe84b6
wlftuc <yshsbhat@outlook.com>
2022-07-22 22:59:39 +0530

    refactor: replace nested function(s) with arrow functions

--- index.ts
+++ index.ts
@@ -7,43 +7,43 @@ import { renderHeader } from "./src/renderHeader";
 dotenv.config();
 
 const twitter = new Twit({
-    consumer_key: `${process.env.CONSUMER_KEY}`,
-    consumer_secret: `${process.env.CONSUMER_SECRET}`,
-    access_token: `${process.env.ACCESS_TOKEN}`,
-    access_token_secret: `${process.env.ACCESS_TOKEN_SECRET}`,
-    timeout_ms: 60 * 1000,
-    strictSSL: true,
+  consumer_key: `${process.env.CONSUMER_KEY}`,
+  consumer_secret: `${process.env.CONSUMER_SECRET}`,
+  access_token: `${process.env.ACCESS_TOKEN}`,
+  access_token_secret: `${process.env.ACCESS_TOKEN_SECRET}`,
+  timeout_ms: 60 * 1000,
+  strictSSL: true,
 });
 
 const out = fs.createWriteStream(__dirname + "/out.png");
 
 (async () => {
-    renderHeader(out);
+  renderHeader(out);
 
-    await new Promise((resolve, reject) => setTimeout(resolve, 3000));
+  await new Promise((resolve, reject) => setTimeout(resolve, 3000));
 
-    const fileToBase64String = fs.readFileSync(__dirname + "/out.png", {
-        encoding: "base64",
-    });
+  const fileToBase64String = fs.readFileSync(__dirname + "/out.png", {
+    encoding: "base64",
+  });
 
-    twitter.post(
-        "media/upload",
-        { media_data: fileToBase64String },
-        function (err, data: any, response) {
-            let media_id = data.media_id_string;
-            twitter.post(
-                "account/update_profile_banner",
-                { media_id: media_id },
-                (err, data, response) => {
-                    if (err) {
-                        console.error(err);
-                    } else {
-                        console.log(chalk.green("Updated twitter header!"));
-                    }
-                }
-            );
+  twitter.post(
+    "media/upload",
+    { media_data: fileToBase64String },
+    (err, data: any, response) => {
+      let media_id = data.media_id_string;
+      twitter.post(
+        "account/update_profile_banner",
+        { media_id: media_id },
+        (err, data, response) => {
+          if (err) {
+            console.error(err);
+          } else {
+            console.log(chalk.green("Updated twitter header!"));
+          }
         }
-    );
+      );
+    }
+  );
 })();
 
 out.on("finish", () => console.log(chalk.green("Finished saving the png!")));
